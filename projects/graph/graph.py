d8a32af3c5a39bbe574f75e21927ac5177c8d5ff
taterntots <matthew.a.weidner@gmail.com>
2020-05-11 17:11:54 -0400

    solved the bfs method

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -116,7 +116,35 @@ class Graph:
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
+        # Create an empty queue and enqueue A PATH TO the starting vertex ID
+        q = Queue()
+        q.enqueue([starting_vertex])
+
+        # Create a Set to store visited vertices
+        visited = set()
+
+        # While the queue is not empty...
+        while q.size() > 0:
+            # Dequeue the first PATH
+            path = q.dequeue()
+            # Grab the last vertex from the PATH
+            v = path[-1]
+            # If that vertex has not been visited...
+            if v not in visited:
+                # CHECK IF IT'S THE TARGET
+                if v == destination_vertex:
+                # IF SO, RETURN PATH
+                    return path
+                else:
+                    # Mark it as visited...
+                    visited.add(v)
+                    # Then add A PATH TO its neighbors to the back of the queue
+                    for next_vert in self.get_neighbors(v):
+                        # _COPY_ THE PATH
+                        p_copy = path.copy()
+                        # APPEND THE NEIGHOR TO THE BACK
+                        p_copy.append(next_vert)
+                        q.enqueue(p_copy)
 
     def dfs(self, starting_vertex, destination_vertex):
         """
