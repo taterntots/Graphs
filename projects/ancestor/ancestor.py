09a2c6a0b302ac0b5ffc985c4398bb93a5ee8536
taterntots <matthew.a.weidner@gmail.com>
2020-05-12 18:53:42 -0400

    fixed the ancestor project to correctly traverse through the graph

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,68 +1,76 @@
 from graph import Graph
-from util import Stack, Queue
+from util import Queue
 
 def earliest_ancestor(ancestors, starting_node):
     # Build the Graph
     graph = Graph()
-    # Add the number of vertices (range 1 to 11 because that's how many exist in the test)
-    for i in range(1, 12):
-        graph.add_vertex(i)
-    # Create linkages from the ancestors list
-    for link in ancestors:
-        graph.add_edge(link[0], link[1])
-    
-    print('----------- GRAPH -----------')
+    # Add the vertices for the parent and child
+    for (parent, child) in ancestors:
+        graph.add_vertex(parent)
+        graph.add_vertex(child)
+        # Create linkages from the ancestors list
+        graph.add_edge(parent, child)
+
     print(graph.vertices)
-
-    # Conduct a breadth-first-search
-    q = Queue()
-    q.enqueue(starting_node)
-
-    # print('Queued Node', q.queue)
-
-    # Define needed arrays
-    popped_children = 0
-    temp_array = []
-    children = []
-
-    # Looping through our graph
-    for node in graph.vertices:
-        # While our node has children
-        while len(graph.vertices[node]) > 0:
-            # Pop each of the children it points to
-            popped_children = graph.vertices[node].pop()
-            # Creates a list of only children with parents (ignores top level nodes)
-            children.append(popped_children)
-            # Creates a list of children with parents, passing the parent as a second argument
-            temp_array.append((popped_children, node))
-
-    print('---------- ARRAYS -----------')
-    print('CHILDREN', children)
-    print('TEMP', temp_array)
-    print('-----------------------------')
-
-    # For each instance in temp_array (list of children with parents)
-    for x in temp_array:
-        # If the first item exists (matches our starting_node)
-        if x[0] == starting_node:
-            print(f'yippeekayay, {x[1]} is the parent of {starting_node}')
-            print('')
-            # If the starting node's parent node is a child (meaning it also has a parent and is not at the top level)
-            if x[1] in children:
-                # Call recursion on that node to find the top
-                return earliest_ancestor(ancestors, x[1])
-            # Otherwise, if the parent is not a child, return the parent (you've reached the top)
-            else:
-                # Returns top level parent (the earliest ancestor)
-                return x[1]
-        
-    # If none of this runs, then the starting_node either does not exist or has no parents
-    return -1
-
-    # NOTE
-    # Since the temp list is already in order from lowest to highest,
-    # If there is more than one ancestor tied to the earliest ancestor
-        # We return the one with the lowest numeric ID
+    return graph.bfs(starting_node)
 
 test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
 print(earliest_ancestor(test_ancestors, 6))
+    
+    # ------------------- BELOW PASSES TEST, BUT IS WRONG ------------------
+
+    # print('----------- GRAPH -----------')
+    # print(graph.vertices)
+
+    # # Conduct a breadth-first-search
+    # q = Queue()
+    # q.enqueue(starting_node)
+
+    # # print('Queued Node', q.queue)
+
+    # # Define needed arrays
+    # popped_children = 0
+    # temp_array = []
+    # children = []
+
+    # # Looping through our graph
+    # for node in graph.vertices:
+    #     # While our node has children
+    #     while len(graph.vertices[node]) > 0:
+    #         # Pop each of the children it points to
+    #         popped_children = graph.vertices[node].pop()
+    #         # Creates a list of only children with parents (ignores top level nodes)
+    #         children.append(popped_children)
+    #         # Creates a list of children with parents, passing the parent as a second argument
+    #         temp_array.append((popped_children, node))
+
+    # print('---------- ARRAYS -----------')
+    # print('CHILDREN', children)
+    # print('TEMP', temp_array)
+    # print('-----------------------------')
+
+    # # For each instance in temp_array (list of children with parents)
+    # for x in temp_array:
+    #     # If the first item exists (matches our starting_node)
+    #     if x[0] == starting_node:
+    #         print(f'yippeekayay, {x[1]} is the parent of {starting_node}')
+    #         print('')
+    #         # If the starting node's parent node is a child (meaning it also has a parent and is not at the top level)
+    #         if x[1] in children:
+    #             # Call recursion on that node to find the top
+    #             return earliest_ancestor(ancestors, x[1])
+    #         # Otherwise, if the parent is not a child, return the parent (you've reached the top)
+    #         else:
+    #             # Returns top level parent (the earliest ancestor)
+    #             return x[1]
+        
+    # # If none of this runs, then the starting_node either does not exist or has no parents
+    # return -1
+
+    # # NOTE
+    # # Since the temp list is already in order from lowest to highest,
+    # # If there is more than one ancestor tied to the earliest ancestor
+    #     # We return the one with the lowest numeric ID
+
+# test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1), (12, 2), (13, 12)]
+# print(earliest_ancestor(test_ancestors, 6))
--- projects/ancestor/graph.py
+++ projects/ancestor/graph.py
@@ -1,288 +1,49 @@
-"""
-Simple graph implementation
-"""
-from util import Stack, Queue  # These may come in handy
+from util import Queue
 
 class Graph:
+  def __init__(self):
+    self.vertices = {}
 
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    def __init__(self):
-        self.vertices = {}
+  def add_vertex(self, int):
+    if not int in self.vertices:
+      self.vertices[int] = set()
 
-    def add_vertex(self, vertex_id):
-        """
-        Add a vertex to the graph.
-        """
-        self.vertices[vertex_id] = set() # Creates a set of edges
+  def add_edge(self, int1, int2):
+    if int1 in self.vertices and int2 in self.vertices:
+      # IMPORTANT! HAD TO FLIP INT2 AND INT1
+      self.vertices[int2].add(int1)
+    else:
+      raise KeyError("Key doesn't exist")
 
-    def add_edge(self, v1, v2):
-        """
-        Add a directed edge to the graph.
-        """
-        # If both vertices are in the graph
-        if v1 in self.vertices and v2 in self.vertices:
-            # Add a one-way link for vertex one to vertex two
-	        self.vertices[v1].add(v2)
-        #Otherwise, provide an error explaining the vertex does not exist
-        else:
-            raise IndexError("Vertex does not exist in graph")
+  def get_neighbors(self, vertex_id):
+    return self.vertices[vertex_id]
 
-    def get_neighbors(self, vertex_id):
-        """
-        Get all neighbors (edges) of a vertex.
-        """
-        return self.vertices[vertex_id]
+  def bfs(self, starting_node):
+    q = Queue()
+    q.enqueue([starting_node])
 
-    def bft(self, starting_vertex):
-        """
-        Print each vertex in breadth-first order
-        beginning from starting_vertex.
-        """
-        # Add the starting_vertex to the queue
-        q = Queue()
-        q.enqueue(starting_vertex)
+    max_path_length = 1
+    earliest_ancestor = -1
 
-        # Keep track of the visited nodes
-        visited = set()
+    while q.size() > 0:
+      # Dequeue the first path
+      path = q.dequeue()
+      # Grab the last vertex from the path
+      v = path[-1]
+      
+      # If the path is longer or equal and the value is smaller, or if the path is longer
+      if (len(path) >= max_path_length and v < earliest_ancestor) or (len(path) > max_path_length):
+        # Set the earliest ancestor to the vert
+        earliest_ancestor = v
+        # Set the max path length to the length of the path
+        max_path_length = len(path)
+      
+      # Loop over each neighbor in the graphs vertices
+      for neighbor in self.get_neighbors(v):
+        # _COPY_ THE PATH
+        p_copy = path.copy()
+        # APPEND THE NEIGHOR TO THE BACK
+        p_copy.append(neighbor)
+        q.enqueue(p_copy)
 
-        # Repeat until queue is empty
-        while q.size() > 0:
-            # Dequeue first vertex
-            v = q.dequeue()
-            # If the vertex is not visited
-            if v not in visited:
-                # Print the vertex in breadth-first order
-                print(v)
-                # Mark the vertex as visited
-                visited.add(v)
-
-                # For the next vertex with linkages, add it to the queue
-                for next_vert in self.get_neighbors(v):
-                    q.enqueue(next_vert)
-
-    def dft(self, starting_vertex):
-        """
-        Print each vertex in depth-first order
-        beginning from starting_vertex.
-        """
-        # Add the starting_vertex to the stack
-        s = Stack()
-        s.push(starting_vertex)
-
-        # Keep track of the visited nodes
-        visited = set()
-
-        # Repeat until stack is empty
-        while s.size() > 0:
-            # Remove the last vertex in the stack
-            v = s.pop()
-            # If the vertex is not visited
-            if v not in visited:
-                # Print the vertex in depth-first order
-                print(v)
-                # Mark the vertex as visited
-                visited.add(v)
-
-                # For the next vertex with linkages, add it to the stack
-                for next_vert in self.get_neighbors(v):
-                    s.push(next_vert)
-
-    def dft_recursive(self, starting_vertex, visited=None):
-        """
-        Print each vertex in depth-first order
-        beginning from starting_vertex.
-
-        This should be done using recursion.
-        """
-        # If visited is empty, set it to an empty set object
-        if visited is None:
-            visited = set()
-
-        # If our starting vertex is not in the visted set
-        if starting_vertex not in visited:
-            # Print the vertex in depth-first order
-            print(starting_vertex)
-            # Mark the vertex as visited
-            visited.add(starting_vertex)
-
-            # For the next vertex with linkages, add it to the stack
-            for next_vert in self.get_neighbors(starting_vertex):
-                self.dft_recursive(next_vert, visited)
-
-
-    def bfs(self, starting_vertex, destination_vertex):
-        """
-        Return a list containing the shortest path from
-        starting_vertex to destination_vertex in
-        breath-first order.
-        """
-        # Create an empty queue and enqueue A PATH TO the starting vertex ID
-        q = Queue()
-        q.enqueue([starting_vertex])
-
-        # Create a Set to store visited vertices
-        visited = set()
-
-        # While the queue is not empty...
-        while q.size() > 0:
-            # Dequeue the first PATH
-            path = q.dequeue()
-            # Grab the last vertex from the PATH
-            v = path[-1]
-            # If that vertex has not been visited...
-            if v not in visited:
-                # CHECK IF IT'S THE TARGET
-                if v == destination_vertex:
-                # IF SO, RETURN PATH
-                    return path
-                else:
-                    # Mark it as visited...
-                    visited.add(v)
-                    # Then add A PATH TO its neighbors to the back of the queue
-                    for next_vert in self.get_neighbors(v):
-                        # _COPY_ THE PATH
-                        p_copy = path.copy()
-                        # APPEND THE NEIGHOR TO THE BACK
-                        p_copy.append(next_vert)
-                        q.enqueue(p_copy)
-
-    def dfs(self, starting_vertex, destination_vertex):
-        """
-        Return a list containing a path from
-        starting_vertex to destination_vertex in
-        depth-first order.
-        """
-        # Create an empty stack and push A PATH TO the starting vertex ID
-        s = Stack()
-        s.push([starting_vertex])
-
-        # Create a Set to store visited vertices
-        visited = set()
-
-        # While the stack is not empty...
-        while s.size() > 0:
-            # Pop the first PATH
-            path = s.pop()
-            # Grab the last vertex from the PATH
-            v = path[-1]
-            # If that vertex has not been visited...
-            if v not in visited:
-                # Check if it's the target
-                if v == destination_vertex:
-                # If so, return PATH
-                    return path
-                # Otherwise, 
-                else:
-                    # Mark it as visited
-                    visited.add(v)
-                    # Then add A PATH TO its neighbors to the top of the stack
-                    for next_vert in self.get_neighbors(v):
-                        # _COPY_ THE PATH
-                        path_copy = path.copy()
-                        # APPEND THE NEIGHOR TO THE BACK
-                        path_copy.append(next_vert)
-                        s.push(path_copy)
-
-    def dfs_recursive(self, starting_vertex, destination_vertex, visited=None, path=None):
-        """
-        Return a list containing a path from
-        starting_vertex to destination_vertex in
-        depth-first order.
-
-        This should be done using recursion.
-        """
-        # If visited is non-existent, set it to an empty set object
-        if visited is None:
-            visited = set()
-
-        # If the path is non-existent, set it to an empty list
-        if path is None:
-            path = []
-        
-        # Add our starting vertex to the visited set and define our path to include it
-        visited.add(starting_vertex)
-        path = path+[starting_vertex]
-
-        # Check if our starting vertex is equal to the destination vertex
-        if starting_vertex == destination_vertex:
-        # If so, return PATH
-            return path
-        # Otherwise, 
-        else:
-            # Add A PATH TO its neighbors to the top of the stack
-            for next_vert in self.get_neighbors(starting_vertex):
-                if next_vert not in visited:
-                    # Recursively call the next vert
-                    path_copy = self.dfs_recursive(next_vert, destination_vertex, visited, path)
-                    if path_copy:
-                        return path_copy
-
-if __name__ == '__main__':
-    graph = Graph()  # Instantiate your graph
-    # https://github.com/LambdaSchool/Graphs/blob/master/objectives/breadth-first-search/img/bfs-visit-order.png
-    graph.add_vertex(1)
-    graph.add_vertex(2)
-    graph.add_vertex(3)
-    graph.add_vertex(4)
-    graph.add_vertex(5)
-    graph.add_vertex(6)
-    graph.add_vertex(7)
-    graph.add_edge(5, 3)
-    graph.add_edge(6, 3)
-    graph.add_edge(7, 1)
-    graph.add_edge(4, 7)
-    graph.add_edge(1, 2)
-    graph.add_edge(7, 6)
-    graph.add_edge(2, 4)
-    graph.add_edge(3, 5)
-    graph.add_edge(2, 3)
-    graph.add_edge(4, 6)
-
-    # print(graph.get_neighbors(4))
-
-    '''
-    Should print:
-        {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
-    '''
-    print(graph.vertices)
-
-    '''
-    Valid BFT paths:
-        1, 2, 3, 4, 5, 6, 7
-        1, 2, 3, 4, 5, 7, 6
-        1, 2, 3, 4, 6, 7, 5
-        1, 2, 3, 4, 6, 5, 7
-        1, 2, 3, 4, 7, 6, 5
-        1, 2, 3, 4, 7, 5, 6
-        1, 2, 4, 3, 5, 6, 7
-        1, 2, 4, 3, 5, 7, 6
-        1, 2, 4, 3, 6, 7, 5
-        1, 2, 4, 3, 6, 5, 7
-        1, 2, 4, 3, 7, 6, 5
-        1, 2, 4, 3, 7, 5, 6
-    '''
-    graph.bft(1)
-
-    '''
-    Valid DFT paths:
-        1, 2, 3, 5, 4, 6, 7
-        1, 2, 3, 5, 4, 7, 6
-        1, 2, 4, 7, 6, 3, 5
-        1, 2, 4, 6, 3, 5, 7
-    '''
-    graph.dft(1)
-    graph.dft_recursive(1)
-
-    '''
-    Valid BFS path:
-        [1, 2, 4, 6]
-    '''
-    print(graph.bfs(1, 6))
-
-    '''
-    Valid DFS paths:
-        [1, 2, 4, 6]
-        [1, 2, 4, 7, 6]
-    '''
-    print(graph.dfs(1, 6))
-    print(graph.dfs_recursive(1, 6))
+    return earliest_ancestor
